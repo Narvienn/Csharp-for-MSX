626326fc49a8aad26406e8255770e14ec43a96b0
Narvienn <40996312+Narvienn@users.noreply.github.com>
2024-09-30 19:14:19 +0200

    Create CSharp-Constants.cs

---
+++ CSharp-Constants.cs
@@ -0,0 +1,6 @@
+//Constants are variables that are unchangeable and read-only - cannot be redeclared
+
+const int myAge = 36;
+myAge = 35; //will produce an error
+
+//const variables cannot be declared empty!
